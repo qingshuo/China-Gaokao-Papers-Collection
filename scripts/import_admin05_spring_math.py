a843aca4ea282a0518f9ae052a889cd7cefe4b12
qingshuo <shuo.li0168@gmail.com>
2026-08-31 17:58:47 +0800

    add reviewed 2020 Shanghai spring math paper

--- scripts/import_admin05_spring_math.py
+++ scripts/import_admin05_spring_math.py
@@ -42,6 +42,11 @@ REVIEWED_PAPERS = {
         "answer_start": 4,
         "source_title": "2019年上海春季高考数学真题",
     },
+    "2020": {
+        "filename": "2020春季上海.pdf",
+        "answer_start": 3,
+        "source_title": "2020年上海市春季高考数学试卷",
+    },
 }
 
 
